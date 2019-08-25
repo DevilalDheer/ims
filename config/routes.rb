8bb7ec5fd11b1a989dacf13e2d6b527a4c8e2971
Moti <devilal25@gmail.com>
2019-08-25 16:39:42 +0530

    Showing seed data in root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :warehouses, only: [:edit, :update]
-  resources :products, only: [:index, :new, :create]
+  resources :warehouses
+  resources :products
   root 'products#index'
 end
