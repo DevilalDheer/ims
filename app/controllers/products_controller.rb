edb0b60e2d82a1a220cb48c631a4de3ed3ccd533
Moti <devilal25@gmail.com>
2019-08-25 16:55:55 +0530

    Update product with associated warehouse products count & thresold

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,36 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update]
+  def index
+  	@warehouses = Warehouse.all
+    @products = Product.includes(warehouse_products: :warehouse)
+  end
+
+  def edit
+    @product.warehouse_products
+  end
+  
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :sku_code, :price,
+      :warehouse_products_attributes => [:item_count, :low_item_threshold, :id])
+  end
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many :warehouse_products
   has_many :warehouses, through: :warehouse_products
+  accepts_nested_attributes_for :warehouse_products
 
   validates :sku_code, uniqueness: true, length: { is: 8 }
 end
